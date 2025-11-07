46de1a26230901dff3e17b5d9b4f1a0caaacb4b1
Yeekong Yang <yyang@icstars.org>
2025-11-07 14:05:14 -0600

    ok

--- task3.py
+++ task3.py
@@ -1,10 +1,11 @@
 #Use the template below to get started
+import requests
 
-url = endpoint
-response = requests.get(l)
+url = https://api.nationalize.io/?name=nathaniel
+response = requests.get(url)
 
-if response.status_code == “ok”:
-data = response.json()
+if response.status_code == "ok":
+data = response.json("name")
 else:
 print(f"Error code: {“error code”}")
 name = data[index of name]
